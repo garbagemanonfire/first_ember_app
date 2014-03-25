c59a0a1c61ce4ce0f71b4ad8a9658b693c2d0131
garbagemanonfire <nilesvm@gmail.com>
2014-03-25 11:07:28 -0700

    Added tests for header, and added header

--- tests/tests.js
+++ tests/tests.js
@@ -25,7 +25,7 @@ test("/", function() {
 
   // helper waiting the application is idle before running the callback
   andThen(function() {
-    equal(find("h2").text(), "Welcome to Ember.js", "Application header is rendered");
-    equal(find("li").length, 3, "There are three items in the list");
+    equal(find("a").text(), "BloggrPostsAbout", "Application header is rendered");
+    equal(find("li").length, 2, "There are two items in the list");
   });
 });
